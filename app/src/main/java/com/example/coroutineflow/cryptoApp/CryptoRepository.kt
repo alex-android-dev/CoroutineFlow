2b34fdab603e9724baea4b3f75f467a192e1018f
avlnkov <alex.lenkov@mail.ru>
2025-03-10 15:45:54 +0300

    добавил передачу Flow в ViewModel

--- app/src/main/java/com/example/coroutineflow/cryptoApp/CryptoRepository.kt
+++ app/src/main/java/com/example/coroutineflow/cryptoApp/CryptoRepository.kt
@@ -1,6 +1,8 @@
 package com.example.coroutineflow.cryptoApp
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
 
 object CryptoRepository {
@@ -8,26 +10,33 @@ object CryptoRepository {
     private val currencyNames = listOf("BTC", "ETH", "USDT", "BNB", "USDC")
     private val currencyList = mutableListOf<Currency>()
 
-    suspend fun getCurrencyList(): List<Currency> {
-        delay(3000)
-        generateCurrencyList()
-        return currencyList.toList()
+    fun getCurrencyFlow(): Flow<List<Currency>> = flow {
+        while (true) {
+            delay(3000) // иммитация долгой загрузки
+
+            generateCurrencyFlow()
+            emit(currencyList.toList())
+
+            delay(3000) // условие бизнес логики. Автообновление должно происходить каждые 3 секунды
+        }
     }
 
-    private fun generateCurrencyList() {
-        val prices = buildList {
+    private fun generateCurrencyFlow() {
+        val prices: List<Int> = buildList {
             repeat(currencyNames.size) {
                 add(Random.nextInt(1000, 2000))
             }
         }
-        val newData = buildList {
+
+        val list = buildList {
             for ((index, currencyName) in currencyNames.withIndex()) {
                 val price = prices[index]
                 val currency = Currency(name = currencyName, price = price)
                 add(currency)
             }
         }
+
         currencyList.clear()
-        currencyList.addAll(newData)
+        currencyList.addAll(list)
     }
 }
--- app/src/main/java/com/example/coroutineflow/cryptoApp/CryptoViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/cryptoApp/CryptoViewModel.kt
@@ -4,7 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
@@ -19,17 +23,22 @@ class CryptoViewModel : ViewModel() {
     }
 
     private fun loadData() {
-        viewModelScope.launch {
-            while (true) {
+        repository.getCurrencyFlow()
+            .onStart {
                 val currentState = _state.value
+
                 if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
                     _state.value = State.Loading
                 }
-                val currencyList = repository.getCurrencyList()
-                _state.value = State.Content(currencyList = currencyList)
-                delay(3000)
             }
-        }
+            // При старте устанавливаем стейт загрузки
+            .filter { listOfCurrency ->
+                listOfCurrency.isNotEmpty()
+            }
+            // Если прилетит пустой список, то он будет отфильтрован и метод onEach вызван не будет
+            .onEach { listOfCurrency ->
+                _state.value = State.Content(listOfCurrency)
+            }
+            .launchIn(viewModelScope)
     }
-
 }
